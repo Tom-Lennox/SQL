d5797fc06b13012e85680ac6f43e48074a15a8f6
Tom-Lennox <dennkousekka1212@gmail.com>
2020-05-09 11:34:25 +0900

    再帰クエリ追加

--- 【SQLServer】shorts_1億insert.sql
+++ 【SQLServer】shorts_1億insert.sql
@@ -2,14 +2,18 @@
 
 use rel
 
+
+--# ▼ ループ処理
+10万件の処理に14秒。論外。
 dbcc dropcleanbuffers
 dbcc freeproccache
 go
 
 waitfor delay '00:00:02'
 
-declare @start_count int = 1
-		,@end_count int = 1000000
+declare @start_count	int = 1
+		--,@end_count		int = 10000000
+		,@end_count		int = 100000
 		,@start_time datetime = getdate()
 		,@processed_time datetime = '1900-01-01 00:00:00.000'
 
@@ -39,3 +43,30 @@ set @processed_time = (getdate() - @start_time)
 select format(@processed_time, 'mm:ss.fff') as '処理時間'
 
 --delete from dbo.customer
+
+--# ▼ 再帰クエリ
+--100万件 ⇒ 11秒くらい。
+DECLARE @insert_count Bigint 
+SELECT @insert_count = 1000000; 
+WITH Base AS
+  (
+    SELECT 1 AS n
+    UNION ALL
+    SELECT n+1 FROM Base WHERE n < @insert_count
+  ),
+  Nums AS
+  (
+    SELECT Row_Number() OVER(ORDER BY n) AS n FROM Base
+  )
+  
+  INSERT INTO customer 
+  　SELECT SYSDATETIME ()
+           ,SYSDATETIME ()
+           ,1
+           ,1
+           ,'customer.taro'
+           ,'customer.taro'
+           ,'29'
+   　FROM Nums  WHERE n <= @insert_count
+
+OPTION (MaxRecursion 0); 
